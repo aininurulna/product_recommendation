d4858dfaf01cd7b50c4c2e11859f2ecfaad7f94b
aininurulna <83540312+aininurulna@users.noreply.github.com>
2021-05-26 15:47:23 +0900

    Update product_recommendation.py

--- product_recommendation.py
+++ product_recommendation.py
@@ -1,62 +1,37 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+#Product Recommendation with KNNWithMeans and SVD
 
 import pandas as pd
 import numpy as np
-
-
-# In[2]:
-
+import seaborn as sns
+from surprise import Dataset
+from surprise import Reader
+import os
+from surprise.model_selection import train_test_split
+from surprise import KNNWithMeans
+from surprise import accuracy
+from surprise import SVD
 
 #import data
 header_list = ['cust', 'product', 'rating', 'timestamp']
-df = pd.read_csv('ratings_Electronics.csv', names=header_list)
-data = df.head(100000)
-
-
-# In[3]:
-
+data = pd.read_csv('ratings_Electronics.csv', names=header_list) #or choose top 100000 if it's too big
 
 #check data information
 data.info()
 data.nunique()
-
-
-# In[4]:
-
 
 #check the distribution of customers for each rating
 import seaborn as sns
 sns.set_theme(style="white")
 ax = sns.countplot(x="rating", data=data)
 
-
-# In[5]:
-
-
 #roughly check the number of products rated per customer
 products_per_customer = data.groupby('cust')['product'].count().sort_values(ascending=False)
 products_per_customer.head(10)
 
-
-# In[6]:
-
-
 #drop the timestamp column
 data.drop(['timestamp'], axis=1,inplace=True)
 
-
-# In[7]:
-
-
-#preparing the data for modeling with KNNWithMeans by Surprise
-from surprise import Dataset
-from surprise import Reader
-import os
-from surprise.model_selection import train_test_split
+#preparing the data for modeling with Surprise
 def prepare (data):
     reader = Reader(rating_scale=(1, 5))
     data2 = Dataset.load_from_df(data,reader)
@@ -65,28 +40,12 @@ def prepare (data):
 train, test = prepare(data)
 
 
-# In[8]:
-
-
-from surprise import KNNWithMeans
-from surprise import accuracy
-knnm = KNNWithMeans(k=5, sim_options={'name': 'pearson_baseline', 'user_based': False})
-
-
-# In[9]:
-
 
 #Memory based collaborative filtering (item-item recommendation) using KNNWithMeans
-from surprise import KNNWithMeans
-from surprise import accuracy
 knnm = KNNWithMeans(k=5, sim_options={'name': 'pearson_baseline', 'user_based': False})
 knnm.fit(train)
 predictionmemory = knnm.test(test)
 rmse_memory = accuracy.rmse(predictionmemory)
-
-
-# In[10]:
-
 
 #check the recommendation system by selecting one customer to recommend 10 products to that customer
 #a little tip, to check the recommendation system, choose the customer who purchased a lot of items by checking the products_per_customer dataframe or pick a random numbre of the index
@@ -94,20 +53,12 @@ custid = list(data['cust'].unique())
 chosen_cust = custid[9343]
 print("Chosen customer " + str(chosen_cust))
 
-
-# In[11]:
-
-
 #list the products purchased by the chosen customer
 purchased = train.ur[train.to_inner_uid(chosen_cust)]
 
 print("Chosen customer purchased the following items ")
 for items in purchased[0]: 
     print(knnm.trainset.to_raw_iid(items))
-
-
-# In[14]:
-
 
 #create a function to return the recommended items by the recommendation system by KNNWithMeans
 def recommendbyknn(purchased, chosen_cust):
@@ -124,29 +75,17 @@ recommendedknndf = pd.DataFrame(recommendedknn, columns=['iid'])
 display(recommendedknndf)
 
 
-# In[15]:
-
 
 #create a model-based collaborative filtering using SVD
-from surprise import SVD
-
 svd = SVD()
 svd.fit(train)
 predictionmodel = svd.test(test)
 rmse_memory = accuracy.rmse(predictionmodel)
 
-
-# In[16]:
-
-
 #list the products purchased by the chosen customer
 print("Chosen customer purchased the following items ")
 for items in purchased[0]: 
     print(svd.trainset.to_raw_iid(items))
-
-
-# In[21]:
-
 
 #create a function to return the recommended items by the recommendation system by SVD
 all_products = list(data['product'].unique())
@@ -164,10 +103,6 @@ recommendedsvddf = pd.DataFrame(recommendedsvd, columns=['uid', 'iid', 'rui', 'e
 recommendedsvddf['err'] = abs(recommendedsvddf.est - recommendedsvddf.rui)
 recommendedsvddf = recommendedsvddf.sort_values(by=['err'], ascending=False)
 recommendedsvddf.head(10)
-
-
-# In[ ]:
-
 
 
 
